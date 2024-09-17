4f678054b608ae78f5f4e76da0b24a70ee10c966
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-09-17 14:36:06 -0500

    Updated the messages on the api endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,6 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World! The project rebuilt!');
+    res.send('Hello, World! This is an express server written in TypeScript!');
 });
 export default router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,6 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req: Request, res: Response) => {
-    res.send('Hello, from users route!');
+    res.send('Hello, from users api route!');
 });
 export default router;
